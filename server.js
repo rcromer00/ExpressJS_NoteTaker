c0eb3ec6ae9a5a9d3bbcc16c128aff61e51c0404
rcromer00 <cromerro@msu.edu>
2020-12-02 09:46:04 -0500

    server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 var express = require("express");
+var cors = require("cors");
 const apiRoutes = require("./routes/api-routes");
 const htmlRoutes = require("./routes/html-routes");
 
@@ -8,12 +9,11 @@ var PORT = process.env.PORT ||  3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.use("/api", apiRoutes);
-app.use("/",htmlRoutes);
-
+app.use("/", htmlRoutes);
+app.use(cors());
 // Listener 
 app.listen(PORT, function() {
     console.log("App listening on PORT" + PORT); 
 });
-
-
